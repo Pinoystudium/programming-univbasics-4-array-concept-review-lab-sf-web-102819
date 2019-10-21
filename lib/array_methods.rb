ca78c651cd5ea2a9ad1d2a7aab6e7b2ab129d4e3
Michael Espina <michael_espina@yahoo.com>
2019-10-21 21:46:53 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,10 @@
 def find_element_index(array, value_to_find)
-  while 
-    	array.length.times { |index|
-    	
-      return nil}
-}
-    
+  array.length.times { |index|
+    	while array[index] != value_to_find
+        return nil
+      else
+        return index
+  }
 end
 
 def find_max_value(array)
